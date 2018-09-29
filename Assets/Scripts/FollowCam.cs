ee4dba308252f7e3e9b3dae989ebfca961bb8f1c
Luis <let65@scaletmail.rutgers.edu>
2018-09-28 20:42:31 -0400

    first

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+    static public GameObject POI;
+
+    [Header("Set Dynamically")]
+    public float easing = 0.05f;
+    public float camZ;
+    
+
+    void Awake()
+    {
+        S = this;
+        camZ = this.transform.position.z;
+    }
+
+
+    void FixedUpdate()
+    {
+        Vector3 destination;
+        if (POI == null)
+        {
+            destination = Vector3.zero;
+        }
+        else
+        {
+            destination = POI.transform.position;
+            if (POI.tag == "Projectile")
+            {
+                if (POI.GetComponent<Rigidbody>().IsSleeping())
+                {
+                    POI = null;
+                    return;
+                }
+            }
+        }
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        destination.z = camZ;
+        transform.position = destination;
+        this.GetComponent<Camera>().orthographicSize = destination.y + 10;
+    }
+
+    
+}
